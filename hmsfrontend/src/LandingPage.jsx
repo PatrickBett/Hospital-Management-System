b02ec8dbaed0ed79eab232b54aed02d5620829a8
Patrick <patrickbett018@gmail.com>
2024-10-21 18:52:01 +0300

    dd

--- hmsfrontend/src/LandingPage.jsx
+++ hmsfrontend/src/LandingPage.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css'
 import Navbar from './Navbar'
 import Login from './Login'
-
+import { Link } from 'react-router-dom'
 function LandingPage() {
   return (
     <div className='container-fluid ' id='landing-page'>
         <h1 className='py-3'>Landing page</h1>
+        <Link to='/hms/mpesa-payment/'>Pay</Link>
 
     </div>
   )
